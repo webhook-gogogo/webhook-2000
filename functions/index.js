48393f3ae7afbfc6b7b50a728eea8be76e82828d
aska <aska0522@gmail.com>
2019-08-16 20:31:43 +0800

    version1(Missing Request Module)

---
+++ functions/index.js
@@ -0,0 +1,74 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+
+
+function getToken(req, res) {
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "20226130ab5abd74f4bac69aae583f77"
+
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+}
+const request = require('request');
+
+
+function ai(req, res) {
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+    // Iterates over each entry - there may be multiple if batched
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+}
+
+
+exports.chatbot = (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      getToken(req, res);
+      break;
+    case 'POST':
+      ai(req, res);
+      break;
+    default:
+      res.status(405).send({ error: 'Something blew up!' });
+      break;
+  }
+};
